52a8727f4cd2da1535f049d59a6f7caf8d32c1a9
loopbackseal <young961027@gmail.com>
2020-12-23 16:13:22 +0900

    brief calculator by js

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
-function sayHello(friend) {
-    console.log("Hello!", friend);
+const calculator = {
+    plus: function(a,b){
+        return a+b;
+    },
+    minus: function(a,b){
+        return a-b;
+    },
+    multiply: function(a,b){
+        return a*b;
+    },
+    divide: function(a,b){
+        return a/b;
+    },
+    pow: function(a,b){
+        return a**b;
+    }
 }
 
-sayHello("Young");
-console.log("Hi!")
+const plus = calculator.plus(5,5)
+const minus = calculator.minus(5,5)
+const multiply = calculator.multiply(5,5)
+const divide = calculator.divide(5,5)
+const pow = calculator.pow(5,5)
+
+console.log(`plus: ${plus}, minus: ${minus}, multiply: ${multiply}, divide: ${divide}, pow: ${pow}`)
