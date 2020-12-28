c1ff3e3425d1245f74c64ede2ef8bfd939a74bd0
loopbackseal <young961027@gmail.com>
2020-12-28 18:37:58 +0900

    basics of if-else

--- index.js
+++ index.js
@@ -1,25 +1,9 @@
-const calculator = {
-    plus: function(a,b){
-        return a+b;
-    },
-    minus: function(a,b){
-        return a-b;
-    },
-    multiply: function(a,b){
-        return a*b;
-    },
-    divide: function(a,b){
-        return a/b;
-    },
-    pow: function(a,b){
-        return a**b;
-    }
+const age = prompt("How old are you?");
+
+if(age >= 18 && age <= 21){
+    console.log("You can drink but you should not");
+} else if(age > 21) {
+    console.log("Go ahead.");
+} else {
+    console.log("You can't drink.");
 }
-
-const plus = calculator.plus(5,5)
-const minus = calculator.minus(5,5)
-const multiply = calculator.multiply(5,5)
-const divide = calculator.divide(5,5)
-const pow = calculator.pow(5,5)
-
-console.log(`plus: ${plus}, minus: ${minus}, multiply: ${multiply}, divide: ${divide}, pow: ${pow}`)
