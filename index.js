452390a7347a335f027f3ebc66f56ec7cebd285d
loopbackseal <young961027@gmail.com>
2020-12-28 23:00:28 +0900

    try online event by reading MDN

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
-const age = prompt("How old are you?");
+const title = document.querySelector("#title");
 
-if(age >= 18 && age <= 21){
-    console.log("You can drink but you should not");
-} else if(age > 21) {
-    console.log("Go ahead.");
-} else {
-    console.log("You can't drink.");
+const BASE_COLOR = "rgb(255, 228, 196)";
+const OTHER_COLOR = "rgb(180, 227, 237)"
+// or "rgb(52, 73, 94)";
+function handleClick() {
+    console.log("It worked")
+    const currentColor = title.style.color;
+    if (currentColor === BASE_COLOR){
+        title.style.color = OTHER_COLOR;
+    } else {
+        title.style.color = BASE_COLOR;
+    }
 }
+
+function init() {
+    title.style.color = BASE_COLOR;
+    title.addEventListener("click", handleClick);
+}
+
+init();
+
+function handleOffline() {
+    console.log("It's offline!");
+}
+function handleOnline() {
+    console.log("It's online");
+}
+window.addEventListener("offline", handleOffline );
+window.addEventListener("online", handleOnline );
