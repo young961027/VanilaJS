d78924b693e58a18da9b3fd9f0d37a34bb91004e
loopbackseal <young961027@gmail.com>
2020-12-29 14:03:48 +0900

    element.classList.toggle() usage with MDN

--- index.js
+++ index.js
@@ -1,30 +1,13 @@
 const title = document.querySelector("#title");
 
-const BASE_COLOR = "rgb(255, 228, 196)";
-const OTHER_COLOR = "rgb(180, 227, 237)";
-//console.log("It worked")
+const CLICKED_CLASS = "clicked";
+
 function handleClick() {
-    const currentColor = title.style.color;
-    if (currentColor === BASE_COLOR){
-        title.style.color = OTHER_COLOR;
-    } else {
-        title.style.color = BASE_COLOR;
-    }
+    title.classList.toggle(CLICKED_CLASS);
 }
 
 function init() {
-    title.style.color = BASE_COLOR;
     title.addEventListener("click", handleClick);
 }
 
 init();
-
-function handleOffline() {
-    console.log("It's offline!");
-}
-function handleOnline() {
-    console.log("It's online");
-}
-
-window.addEventListener("offline", handleOffline );
-window.addEventListener("online", handleOnline );
