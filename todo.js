01e2663130c9bf6c5c046a7ce7afcfd875ad7253
loopbackseal <young961027@gmail.com>
2021-01-05 18:07:33 +0900

    Delete <li> by click delBtn

--- todo.js
+++ todo.js
@@ -4,7 +4,19 @@ toDoList = document.querySelector(".js-toDoList");
 
 const TODOS_LS = "toDos";
 
-const toDos = [];
+let toDos = [];
+
+function deleteToDo(event) {
+    const btn = event.target;
+    const li = btn.parentNode;
+    toDoList.removeChild(li);
+    const cleanToDos = toDos.filter(function(toDo){
+        console.log(toDo.id,li.id);
+        return toDo.id !== parseInt(li.id);
+    })
+    toDos = cleanToDos;
+    saveToDos();
+}
 
 function saveToDos() {
     localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
@@ -16,9 +28,11 @@ function paintToDo(text) {
     const span = document.createElement("span");
     const newId = toDos.length + 1;
     delBtn.innerText = "❌";
+    delBtn.addEventListener("click",deleteToDo);
     span.innerText = text
     li.appendChild(delBtn);
     li.appendChild(span);
+    li.id = newId;
     toDoList.appendChild(li);
     const toDoObj = {
         text: text,
